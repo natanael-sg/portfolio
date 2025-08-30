3f589b29baec3cd6c5a2ad960b84facf3bcfed47
Natanael9999 <118235069+Natanael9999@users.noreply.github.com>
2025-08-29 21:59:11 -0300

    segundo commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,32 +6,19 @@ export default function Home(){
         <>
         <main>
             <section id='home' className={styles.home}>
-                <header className={styles.header}>
-                    <div className={styles.titleHeader}>
-                        <h1 className={styles.logoHeader}>Natanael</h1>
-                        <h1 className={styles.logoHeader}>☰</h1>
+                <div className={styles.homeTitle}>
+
+                    <h1>Natanael</h1>
+                    
+                    <div className={styles.descricaoContainer}>
+                        <div className={styles.carrosselDescricao}>
+                            <p className={styles.descricao}>Desenvolvedor Front-End</p>
+                            <p className={styles.descricaoPonto}>●</p>
+                        </div>
                     </div>
-                    {/*<div className={styles.navigation}>
-                        <ul className={styles.menu}>
-                            <li>
-                                <Link href={"/sobre"}>Sobre</Link>
-                            </li>
-                            <li>
-                                <Link href={"/projetos"}>Projetos</Link>
-                            </li>
-                            <li>
-                                <Link href={"/contato"}>Contato</Link>
-                            </li>
-                        </ul>
-                    </div>*/}
-                </header>
-                <div className={styles.homeContainer}>
-                    <div>
-                        <span className={styles.title}><h1 className={styles.colorWhite}>Olá, eu sou </h1><h1>Natanael</h1></span>
-                    </div>
-                    <div>
-                        <p className={styles.normalText}>Desenvolvedor Front-End em React e Next.js</p>
-                    </div>
+
+                    <h1>Santos</h1>
+                    
                 </div>
             </section>
         </main>
